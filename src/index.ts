ffe4753687c79300b1d43bc4f5669a811e429cdf
Terry Fei <i@terryfei.com>
2022-07-08 03:06:13 +0800

    new hfc interface

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import {
   useRef,
-  useEffect,
+  createElement,
   ComponentClass,
+  useLayoutEffect,
   FunctionComponent,
   createElement as h,
 } from "react";
 import * as ReactDom from "react-dom";
 
 const slotComponent =
-  (renderSlot: (container: HTMLElement, args: any) => void) => (ps: any) => {
+  (renderSlot: (container: HTMLElement, args: any) => void) =>
+  (ps: Record<string, any>) => {
     const ref = useRef<HTMLElement>(null);
-    useEffect(() => {
+    useLayoutEffect(() => {
       renderSlot(ref.current!, ps);
     });
 
@@ -20,72 +22,48 @@ const slotComponent =
     return h(ps.__tag || "div", props, null);
   };
 
+function toReactProps(props: HfcProps) {
+  const reactProps = { ...props.attrs, ...props.events, ...props.others };
+
+  const slotKeys = Object.keys(props.slots);
+  for (let i = 0; i < slotKeys.length; i++) {
+    const key = slotKeys[i];
+    reactProps[key] = slotComponent(props.slots[key]);
+    reactProps[key].render = props.slots[key];
+  }
+
+  return reactProps;
+}
+
 export default function reactToHfc(
   Comp: ComponentClass | FunctionComponent,
   opts: {
     tag: string;
-    connected: (container: HTMLElement, props: HfcProps) => void;
-    disconnected: () => void;
+    disconnected?: () => void;
   }
 ): typeof HyperFunctionComponent {
   class HfcComponent {
     static tag: string;
-    state: Record<string, any> = {};
     reactRoot: any;
-    container!: HTMLElement;
     RC = Comp;
-    constructor(public props: HfcProps) {
-      const attrKeys = Object.keys(props.attrs);
-      for (let i = 0; i < attrKeys.length; i++) {
-        const key = attrKeys[i];
-        this.state[key] = props.attrs[key];
-      }
-
-      const eventKeys = Object.keys(props.events);
-      for (let i = 0; i < eventKeys.length; i++) {
-        const key = eventKeys[i];
-        this.state[key] = props.events[key];
-      }
-
-      const slotKeys = Object.keys(props.slots);
-      for (let i = 0; i < slotKeys.length; i++) {
-        const key = slotKeys[i];
-        this.state[key] = slotComponent(props.slots[key]);
-        this.state[key].render = props.slots[key];
-      }
-    }
-    connected(container: HTMLElement) {
-      this.container = container;
-
-      if (opts.connected) opts.connected(this.container, this.props);
+    constructor(public container: HTMLElement, props: HfcProps) {
+      const reactProps = toReactProps(props);
 
       const createRoot = (ReactDom as any).createRoot;
       if (createRoot) {
         this.reactRoot = createRoot(container);
-        this.reactRoot.render(h(Comp, this.state));
+        this.reactRoot.render(h(Comp, reactProps));
       } else {
-        ReactDom.render(h(Comp, this.state), container);
+        ReactDom.render(h(Comp, reactProps), container);
       }
     }
-    changed(
-      type: "attr" | "event" | "slot",
-      name: string,
-      oldValue: any,
-      newValue: any
-    ) {
-      if (type === "attr" || type === "event") {
-        this.state[name] = newValue;
-      }
-
-      if (type === "slot") {
-        this.state[name] = slotComponent(newValue);
-        this.state[name].render = newValue;
-      }
+    changed(props: HfcProps) {
+      const reactProps = toReactProps(props);
 
       if (this.reactRoot) {
-        this.reactRoot.render(h(Comp, this.state));
+        this.reactRoot.render(h(Comp, reactProps));
       } else {
-        ReactDom.render(h(Comp, this.state), this.container);
+        ReactDom.render(h(Comp, reactProps), this.container);
       }
     }
     disconnected() {
@@ -102,4 +80,16 @@ export default function reactToHfc(
   HfcComponent.tag = opts.tag;
 
   return HfcComponent;
+}
+
+export function toHfcSlot(
+  Component: React.FunctionComponent | React.ComponentClass
+) {
+  return (props: any) => {
+    return createElement(
+      props.__tag || "div",
+      props.__props,
+      createElement(Component, props)
+    );
+  };
 }
